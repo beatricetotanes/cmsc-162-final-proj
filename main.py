1c0c7b0b6470c1ab4fc454e74c6ea8d55af99955
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2023-01-04 14:44:37 +0800

    main file

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ root = tk.Tk()
 root.title('Programming Project') # IDE Title
 root.geometry('1280x720') # Size of Window
 
+# Gets main window size
+root_width = root.winfo_width()
+root_height = root.winfo_height()
+
 root.update()
 
 # root.config(menu=menu_bar) # Configuring the menu bar
