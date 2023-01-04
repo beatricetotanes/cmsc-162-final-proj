be7c6f8459045a5913ca8f8eed4c29b43be10ddf
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2023-01-04 14:18:05 +0800

    Created root window

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+
+#  Root window
+root = tk.Tk()
+root.title('Programming Project') # IDE Title
+root.geometry('1280x720') # Size of Window
+
+root.update()
+
+# root.config(menu=menu_bar) # Configuring the menu bar
+root.mainloop()
